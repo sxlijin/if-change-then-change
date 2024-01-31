6febcb499776ef5ebec262920266261adfb34ad9
Sam Lijin <dev@sxlijin.com>
2024-01-30 18:32:15 -0700

    more test cases

--- tests/data/basic/c.sh
+++ tests/data/basic/c.sh
@@ -2,5 +2,5 @@ export LOG="thumbnail=debug;video=info;"
 # if-change
 export THUMBNAIL_BUCKET="s3://video-thumbnails/"
 export VIDEO_BUCKET="s3://video-service/"
-# then-change b.sh
+# then-change d.sh
 export VIDEO_CONFIG="video-service/config.json"
---
+++ tests/data/basic/d.sh
@@ -0,0 +1,4 @@
+echo "building video service"
+echo "indexing video assets"
+echo "serving from s3://video-service/ and s3://video-thumbnails/"
+echo "service started on port 0000"
